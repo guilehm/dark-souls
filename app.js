020441367b3d50c0a7a054664bedf30d289f507d
Guilherme Luis <guile.hm@hotmail.com>
2019-07-19 01:02:46 -0300

    improve app

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const Discord = require('discord.js')
 const token = process.env.DISCORD_TOKEN
+const request = require('request')
+const getUserStats = require('./helpers/get-user-stats')
 
 const client = new Discord.Client()
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
+    let handleResponse = data => {
+        let stats = data
+        if (!stats.vitorias) {
+            msg.channel.send('Deu bosta, tente novamente!')
+        } else {
+            let response = `
+                kills: ${stats.kills}\nvitórias: ${stats.vitorias}\nkd: ${stats.kd}\npartidas: ${stats.partidas}`
+            msg.channel.send(response)
+        }
+    }
+
     if (msg.content === '.ping') {
         msg.channel.send(`Oi, ${msg.author}, estou conectado!`)
+    }
+
+    if (msg.content.startsWith('.s ')) {
+        let username = msg.content.split(' ').pop()
+        await getUserStats(username).then(handleResponse, handleResponse)
     }
 })
 
