4a4b4ea778183c1382c989a5319980c1e61c43d0
Guilherme Luis <guile.hm@hotmail.com>
2019-07-19 01:14:36 -0300

    create bot controller

---
+++ controllers/bot-controller.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const token = process.env.DISCORD_TOKEN
+
+const getUserStats = require('../helpers/get-user-stats')
+
+const client = new Discord.Client()
+
+module.exports = (req, res) => {
+    client.on('message', async (msg) => {
+        let handleResponse = data => {
+            let stats = data
+            if (!stats.vitorias) {
+                msg.channel.send('Deu bosta, tente novamente!')
+            } else {
+                let response = `
+                    kills: ${stats.kills}\nvitórias: ${stats.vitorias}\nkd: ${stats.kd}\npartidas: ${stats.partidas}`
+                msg.channel.send(response)
+            }
+        }
+
+        if (msg.content === '.ping') {
+            msg.channel.send(`Oi, ${msg.author}, estou conectado!`)
+        }
+
+        if (msg.content.startsWith('.s ')) {
+            let username = msg.content.split(' ').pop()
+            await getUserStats(username).then(handleResponse, handleResponse)
+        }
+    })
+
+    client.login(token)
+}
