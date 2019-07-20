0d68904be42cb41e48eb19bc2f199c948a7f4816
Guilherme Luis <guile.hm@hotmail.com>
2019-07-20 20:59:48 -0300

    add stats endpoint to app

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const DEBUG = process.env.DEBUG
 const PORT = process.env.PORT || 4000
 
 const app = new express()
+
 const botController = require('./controllers/bot-controller')
+const cleanStatsController = require('./controllers/clean-stats-controller')
 
 app.get('/', botController)
+app.get('/stats/', cleanStatsController)
 
 app.listen(PORT, () => {
     let message = DEBUG ? 'Starting development server on port' : 'App listening on port'
