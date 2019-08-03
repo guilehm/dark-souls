24d793b43a727d0abdf64b1e2a0e36b3fc3e0808
Guilherme Luis <guile.hm@hotmail.com>
2019-08-03 09:55:28 -0300

    fixes for lint

--- helpers/get-user-stats.js
+++ helpers/get-user-stats.js
@@ -1,27 +1,27 @@
-const request = require('request')
+const request = require('request');
 
 
 async function getUserStats(username, req) {
-    let hostname = req.headers.host
-    let protocol = req.connection.encrypted ? 'https' : 'http'
-    let cleanStatsApi = `${protocol}://${hostname}/stats/`
+    let hostname = req.headers.host;
+    let protocol = req.connection.encrypted ? 'https' : 'http';
+    let cleanStatsApi = `${protocol}://${hostname}/stats/`;
 
     let options = {
         url: cleanStatsApi,
-        qs: { username: username},
-    }
+        qs: { username: username },
+    };
     return new Promise((resolve, reject) => {
         request(options, (error, response, body) => {
-            if (error) reject(error)
+            if (error) reject(error);
             try {
-                data = JSON.parse(body)
+                data = JSON.parse(body);
             } catch (err) {
-                reject(err)
+                reject(err);
             }
-            data.username = username
-            resolve(data)
-        })
-    })
+            data.username = username;
+            resolve(data);
+        });
+    });
 }
 
-module.exports = getUserStats
+module.exports = getUserStats;
