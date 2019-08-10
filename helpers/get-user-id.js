29aed99de0a938fece0595e0de60f30841ffad01
Guilherme Luis <guile.hm@hotmail.com>
2019-08-10 09:56:23 -0300

    fixes for lint

--- helpers/get-user-id.js
+++ helpers/get-user-id.js
@@ -1,22 +1,22 @@
-const request = require('request')
+const request = require('request');
 
 
 async function getUserId(username) {
 
-    const uidApi = process.env.UID_API
+    const uidApi = process.env.UID_API;
 
     let options = {
         url: uidApi,
         qs: { username: username},
-    }
+    };
 
     return new Promise((resolve, reject) => {
         request(options, (error, response, body) => {
-            if (error) reject(error)
-            data = JSON.parse(body)
-            resolve(data.uid)
-        })
-    })
+            if (error) reject(error);
+            data = JSON.parse(body);
+            resolve(data.uid);
+        });
+    });
 }
 
-module.exports = getUserId
+module.exports = getUserId;
