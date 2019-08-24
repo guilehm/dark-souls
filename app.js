c6d4f238cd41820d24ec79d7dec93df1c1d7ef5a
guilherme <guile.hm@hotmail.com>
2019-08-24 09:58:11 -0300

    add healthcheck to app

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = new express();
 
 const botController = require('./controllers/bot-controller');
 const cleanStatsController = require('./controllers/clean-stats-controller');
+const healthcheckController = require('./controllers/healthcheck-controller');
 
 app.get('/', botController);
 app.get('/stats/', cleanStatsController);
+app.get('/healthcheck/', healthcheckController);
 
 app.listen(PORT, () => {
     let message = DEBUG ? 'Starting development server on port' : 'App listening on port';
