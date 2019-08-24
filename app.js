1d2f12663a664a7ec0ce9857ab043ffad24ba7f8
Guilherme <guile.hm@hotmail.com>
2019-08-24 16:54:27 -0300

    add self endpoint and request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const logger = require('heroku-logger');
+const request = require('request');
 
 const DEBUG = process.env.DEBUG;
 const PORT = process.env.PORT || 4000;
+const SELF_ENDPOINT = process.env.SELF_ENDPOINT;
 
 const app = new express();
 
@@ -19,4 +21,7 @@ app.get('/healthcheck/', healthcheckController);
 app.listen(PORT, () => {
     let message = DEBUG ? 'Starting development server on port ' : 'App listening on port ';
     logger.info(message + `${PORT}`);
+    if (SELF_ENDPOINT) {
+        request.get(SELF_ENDPOINT);
+    }
 });
