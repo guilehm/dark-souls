4885e49649ca41b4035e8e333245e6c069fe0a6c
Guilherme Luis <guile.hm@hotmail.com>
2019-08-27 23:05:30 -0300

    improve stocks controller

--- controllers/stocks-controller.js
+++ controllers/stocks-controller.js
@@ -23,11 +23,11 @@ module.exports = async (req, res) => {
     if (!stock) return handleError('Stock is required', 'Por favor preencha o código da ação.');
 
     let url = `${stocksEndpoint}stock/analysis/`;
+
     request(url, (err, response, body) => {
-        if (response.statusCode !== 200){
-            return handleError('Error at Stocks API', 'Ops.. Ocorreu um erro na API de Ações.');
-        }
+        let errorMessage = 'Ops... Ocorreu um erro na API de Ações.';
+        if (response.statusCode !== 200) return handleError('Error at Stocks API', errorMessage);
+        if (err) return handleError(err.message, errorMessage);
         return handleSuccess(body);
     });
-
 };
