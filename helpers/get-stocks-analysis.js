85140549b4ade0dd37e4d3b35fc3f606c82a2dc9
Guilherme Luis <guile.hm@hotmail.com>
2019-08-27 23:56:12 -0300

    improve get stocks analysis helper

--- helpers/get-stocks-analysis.js
+++ helpers/get-stocks-analysis.js
@@ -2,32 +2,28 @@ const request = require('request');
 const logger = require('heroku-logger');
 
 
-function getStockAnalysis(stock) {
+async function getStockAnalysis(stock) {
     const stocksEndpoint = process.env.STOCKS_ENDPOINT;
 
-    let handleError = (error, message) => {
-        logger.error(error);
-        return JSON.stringify({
-            error: true,
-            message: message,
+
+    return new Promise((resolve, reject) => {
+
+        let url = `${stocksEndpoint}stocks/${stock}/analysis/`;
+
+        request(url, (err, response, body) => {
+            let errorMessage = 'Ops... Ocorreu um erro na API de Ações.';
+            if (err) reject(err.message);
+            if (response.statusCode === 404) reject('Ação não encontrada!');
+            if (response.statusCode !== 200) reject(errorMessage);
+            try {
+                parsedData = JSON.parse(body);
+            } catch (e) {
+                resolve(errorMessage);
+            }
+            resolve(JSON.stringify(
+                parsedData.company
+            ));
         });
-        
-    };
-
-    let handleSuccess = data => {
-        return JSON.stringify(data);
-    };
-
-    if (!stocksEndpoint) return handleError('Stocks Endopint not set.', 'Ops... Ocorreu um erro.');
-    if (!stock) return handleError('Stock is required', 'Por favor preencha o código da ação.');
-
-    let url = `${stocksEndpoint}stock/analysis/`;
-
-    request(url, (err, response, body) => {
-        let errorMessage = 'Ops... Ocorreu um erro na API de Ações.';
-        if (response.statusCode !== 200) return handleError('Error at Stocks API', errorMessage);
-        if (err) return handleError(err.message, errorMessage);
-        return handleSuccess(body);
     });
 }
 
